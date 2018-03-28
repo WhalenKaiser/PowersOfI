e9dfb471bd4ee82f9b0b6ac6c50b7d7fe31daf8e
Michelle Kaiser <whalen.michelle@gmail.com>
2018-03-28 14:07:16 +0100

    Create powersOfI_test.py

---
+++ powersOfI_test.py
@@ -0,0 +1,12 @@
+Test.it('Basic Tests')
+Test.assert_equals(pofi(0),'1');
+Test.assert_equals(pofi(1),'i');
+Test.assert_equals(pofi(2),'-1');
+Test.assert_equals(pofi(3),'-i');
+Test.assert_equals(pofi(4),'1');
+Test.assert_equals(pofi(5),'i');
+Test.assert_equals(pofi(6),'-1');
+Test.assert_equals(pofi(7),'-i');
+Test.assert_equals(pofi(8),'1');
+Test.assert_equals(pofi(9),'i');
+Test.assert_equals(pofi(10),'-1');
